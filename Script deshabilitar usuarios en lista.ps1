46a06ef41a0f8929fff30851b4fd990bfdfebfe1
JesusMendez24 <114131470+JesusMendez24@users.noreply.github.com>
2023-08-28 16:56:46 -0300

    Add files via upload

---
+++ Script deshabilitar usuarios en lista.ps1
@@ -0,0 +1,5 @@
+﻿$usuario = "ct-abenza" # Reemplaza "nombre_de_usuario" por el nombre de usuario del que deseas obtener los grupos
+
+$grupos = Get-ADUser -Identity $usuario | Get-ADPrincipalGroupMembership
+
+$grupos | Select-Object -ExpandProperty Name | Out-File -FilePath "C:\Users\ct-jmendez\Desktop\grupos_$usuario.txt"
