4dbbddf5c47822998de177ee22aed264beb62605
JesusMendez24 <114131470+JesusMendez24@users.noreply.github.com>
2023-08-28 16:57:07 -0300

    Add files via upload

---
+++ script para mapear todo las maquinas del domino.ps1
@@ -0,0 +1,15 @@
+﻿$computers = Get-ADComputer -Filter *
+$domain = "latam.com"
+$username = "ct-ealba"
+
+foreach ($computer in $computers) {
+    $computerName = $computer.Name
+    $group = [ADSI]"WinNT://$computerName/Administrators,group"
+    $members = $group.Invoke("Members") | foreach { $_.GetType().InvokeMember("Name", 'GetProperty', $null, $_, $null) }
+    
+    if ($members -contains $username) {
+        Write-Host "El usuario $username es administrador en $computerName"
+    } else {
+        Write-Host "El usuario $username NO es administrador en $computerName"
+    }
+}
